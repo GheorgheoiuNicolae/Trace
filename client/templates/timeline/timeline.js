768e7f3fe017d1609b0c313efd3b50cf925ec9e8
Gheorgheoiu Nicolae <nicolae.gheorgheoiu@businessmail.avelon-cetex.com>
2016-05-02 15:04:17 +0300

    Upgrade to meteor 1.3 and Sorted the days correctly.

--- client/templates/timeline/timeline.js
+++ client/templates/timeline/timeline.js
@@ -1,12 +1,10 @@
 Meteor.subscribe('entries');
 Meteor.subscribe('getDayCategorisedEntries');
 
-
-
 var today = moment().format('YYYY-MM-DD');
 Session.set('currentDate', today);
 //var currentDate = Session.get('currentDate');
-console.log('!!!today', today);
+console.log('today', today);
 
 Template.timeline.events({
     'submit .quickAdd': function(event, template){
@@ -29,9 +27,25 @@ Template.timeline.events({
 
 Template.timeline.helpers({
     sortedEntries: ()=> {
+        // In addition to returning the entries sorted by day, I also needed to sort the days. 
+        // The problem with this is that in the agregation I could only keep one key and that is 
+        // the _id which keeps a date as a string like this: "2106-02-16"
+
+        // The code below converts that _id into a date.getTime() and after that, using undersore, I filtered the days.
+
+        
         var result = SortedEntries.find({});
-        console.log('Sorted entries',result.fetch());
-        return result;
+        var fetchedEntries = result.fetch();
+        
+        for(i = 0; i < fetchedEntries.length; i++) {
+            var dayId = fetchedEntries[i]._id;
+            var splitted = dayId.split('-');
+            var dayDate = new Date(splitted[0], splitted[1], splitted[2]).getTime();
+            fetchedEntries[i].date = dayDate;
+        }
+
+        var entries = _.sortBy(fetchedEntries, 'date');
+        return entries;
     }
     // isToday: ()=> {
     //     var formattedDate = Session.get('formattedDate');
